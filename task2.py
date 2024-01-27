f6027ea17fedb6703c481f127d8eaf7ab4d825fc
volodymyrdubrovskyi <vladimir.dubrovskiy@gmail.com>
2024-01-27 16:04:55 +0200

    version 27.01.2024.16.04

--- task2.py
+++ task2.py
@@ -1,7 +1,7 @@
 from node_class_func import Node
 
-def min_node_val(node: Node):
-    current = node
+def min_node_val(root: Node):
+    current = root
     while current.left:
         current = current.left
     return current
--- task3.py
+++ task3.py
@@ -1,9 +1,9 @@
 from node_class_func import Node
 
-def sum_node_val(node: Node):
-    summa = node.val
-    if node.left:
-        summa += sum_node_val(node.left)
-    if node.right:
-        summa += sum_node_val(node.right)
+def sum_node_val(root: Node):
+    summa = root.val
+    if root.left:
+        summa += sum_node_val(root.left)
+    if root.right:
+        summa += sum_node_val(root.right)
     return summa
